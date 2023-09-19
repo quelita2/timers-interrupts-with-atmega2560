32b84b06aa2b26fdb821bfd18ea49e7df4c8036f
Thiago Lopes do Nascimento <thiagonasmento20@gmail.com>
2023-09-19 17:51:31 -0300

    start repository and add timer-interrupts.c

---
+++ src/timer-interrupts.c
@@ -0,0 +1,24 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+
+int main(void){              
+  DDRG = 0b00100000;
+  TCCR0A = 0b00000000;
+  TCCR0B = 0b00000001;
+  TIMSK0 = 0b00000001;
+  sei(); 
+  
+  while(1){ }
+  
+  return 0;
+}
+
+ISR(TIMER0_OVF_vect){
+  if(PING & 0b00100000){
+    PORTG = 0b00000000;
+  }else{
+    PORTG = 0b00100000;
+  }
+}
